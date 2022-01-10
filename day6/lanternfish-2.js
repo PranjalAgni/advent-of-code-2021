3cbf821b2c2f36635bbdd45ca1a7edda44fba32f
PranjalAgni <pranjal.agnihotri6@gmail.com>
2022-01-11 00:03:13 +0530

    :seedling: solved lanternfish part 2

--- day6/lanternfish-2.js
+++ day6/lanternfish-2.js
@@ -6,7 +6,6 @@ const toMap = (data) => {
   data.forEach((d) => {
     if (!hashMap.has(d)) {
       hashMap.set(d, 0);
-      fish.push(d);
     }
     hashMap.set(d, hashMap.get(d) + 1);
   });
@@ -15,25 +14,31 @@ const toMap = (data) => {
 };
 
 const incrementMap = (map, timer, count) => {
-  map.set(timer, count);
+  if (!map.has(timer)) map.set(timer, 0);
+  map.set(timer, map.get(timer) + count);
 };
 const solve = (data, days) => {
-  const fishMap = toMap(data);
+  let fishMap = toMap(data);
 
   for (let day = 0; day < days; day++) {
-    let N = fish.length;
     const nextMap = new Map();
     for (const [timer, count] of fishMap) {
       if (timer > 0) {
         incrementMap(nextMap, timer - 1, count);
       } else {
         incrementMap(nextMap, 6, count);
-        incrementMap(nextMap, 8, nextMap.has(8) ? nextMap.get(8) : 1);
+        incrementMap(nextMap, 8, count);
       }
     }
+
+    fishMap = nextMap;
   }
 
-  return fish.length;
+  let answer = 0;
+  for (const [_timer, count] of fishMap) {
+    answer += count;
+  }
+  return answer;
 };
 (async () => {
   const INPUT_PATH = path.join(__dirname, 'input.txt');
